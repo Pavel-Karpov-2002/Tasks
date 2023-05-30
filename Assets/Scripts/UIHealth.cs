fe7ab18be9b3fe3efd4da2f8085a66324521ec7a
Pavel-Karpov-2002 <carpowigor@yandex.by>
2023-05-30 15:07:37 +0300

    Corrections.

--- Assets/Scripts/UIHealth.cs
+++ Assets/Scripts/UIHealth.cs
@@ -4,33 +4,37 @@ using UnityEngine.UI;
 
 public class UIHealth : MonoBehaviour
 {
-    [SerializeField] private Slider healthSlider; // ползунок
-    [SerializeField] private Player player; // объкт игрока
-    [SerializeField] private float healthSliderSpeed; // скорость изменения значения value у ползунка
+    [SerializeField] private Slider _healthSlider; // ползунок
+    [SerializeField] private Player _player; // объкт игрока
+    [SerializeField] private float _healthSliderSpeed; // скорость изменения значения value у ползунка
 
-    private IEnumerator _healthBarCoroutine; // корутина для изменения health bar'a
+    private Coroutine _healthBarCoroutine; // корутина для изменения health bar'a
 
     private void Start()
     {
-        healthSlider.maxValue = player.MaxHealth; // задает максимальное value для ползунка равное максимальному здоровью игрока 
-        healthSlider.value = player.Health; // задает начальное значение ползунка на количество здоровья при старте сцены
-        player.OnHealthChange += UpdateHealthBar; // добавлем к ивенту Action функцию
+        _healthSlider.maxValue = _player.MaxHealth; // задает максимальное value для ползунка равное максимальному здоровью игрока 
+        _healthSlider.value = _player.Health; // задает начальное значение ползунка на количество здоровья при старте сцены
+        _player.OnHealthChange += OnHealthBarChange; // добавлем к ивенту Action функцию
     }
 
-    private void UpdateHealthBar(int health)
+    private void OnHealthBarChange(int health)
     {
         if (_healthBarCoroutine != null) // в случае, если корутина равняется null не вызывается остановка
             StopCoroutine(_healthBarCoroutine); // остановка старой корутины
-        _healthBarCoroutine = ChangeHealthBar(health); // установка новоого значения для корутины
-        StartCoroutine(_healthBarCoroutine); // запуск корутины
+        _healthBarCoroutine = StartCoroutine(ChangeHealthBar(health)); // запуск корутины
     }
 
     private IEnumerator ChangeHealthBar(int endValue)
     {
-        while(healthSlider.value != endValue) // пока слайдер здоровья не равен конечному значению (здоровью игрок) цикл не завершается
+        while(_healthSlider.value != endValue) // пока слайдер здоровья не равен конечному значению (здоровью игрок) цикл не завершается
         {
-            yield return new WaitForFixedUpdate(); // продолжает корутину как при обновлении FixedUpdate
-            healthSlider.value = Mathf.MoveTowards(healthSlider.value, endValue, Time.deltaTime * healthSliderSpeed); // плавно изменяет значение слайдера здоровья
+            yield return null; // продолжает корутину
+            _healthSlider.value = Mathf.MoveTowards(_healthSlider.value, endValue, Time.deltaTime * _healthSliderSpeed); // плавно изменяет значение слайдера здоровья
         }
+    }
+
+    private void OnDestroy()
+    {
+        _player.OnHealthChange -= OnHealthBarChange;
     }
 }
